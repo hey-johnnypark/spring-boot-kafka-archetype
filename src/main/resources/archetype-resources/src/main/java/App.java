f5179d3166b81f2cea91fa0b855e6e9f2d14a8ff
Johnny Park <johnnypark@mailbox.org>
2018-01-13 13:13:58 +0100

    Adds producer

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -3,25 +3,17 @@ package ${groupId};
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 class App {
 
-  private static final Logger LOG = LoggerFactory.getLogger(App.class);
-
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
-  }
-
-  @KafkaListener(topics = "${kafka.topic}")
-  public void onMessage(ConsumerRecord<String, String> record) {
-    LOG.info("Received record: {}", record.value());
-
-    // apply-your-logic-to (record)
-
   }
 
 }
